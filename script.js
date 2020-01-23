a3875abcb44b40ccc472b1e98ab0e8742e993f7d
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2020-01-23 14:19:23 +0100

    Aggiunti file di base

---
+++ script.js
@@ -0,0 +1,66 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateForm() {
+    var error = $('#error');
+    var firstname = $('#firstname').val().trim();
+    var lastname = $('#lastname').val().trim();
+    var username = $('#username').val().trim();
+    var password = $('#password').val().trim();
+    var confirmPassword = $('#confirm-password').val().trim();
+    var email = $('#email').val().trim();
+
+    // Validate firstname
+    if (firstname.length < 4) {
+        error.text('First name not valid!');
+        error.show();
+        return false;
+    }
+
+    // Validate lastname
+    if (lastname.length < 4) {
+        error.text('Last name not valid!');
+        error.show();
+        return false;
+    }
+
+    // Validate username
+    if (username.length < 8 || username.length > 20) {
+        error.text('Username name not valid!');
+        error.show();
+        return false;
+    }
+
+    // Validate password and confirm password
+    if (password != confirmPassword) {
+        error.text('Passwords do not match!');
+        error.show();
+        return false;
+    }
+
+    // Validate email
+    if (!validateEmail(email)) {
+        error.text('Email not valid!');
+        error.show();
+        return false;
+    }
+
+    // Return true because the form is valid
+    return true;
+}
+
+
+$('#registration-form').submit(function (event) {
+    // event.preventDefault();
+
+    if (!validateForm()) {
+        event.preventDefault();
+    }
+});
+
+$('.input').keyup(function () {
+    var value = $(this).val().trim();
+    $(this).val(value);
+})
